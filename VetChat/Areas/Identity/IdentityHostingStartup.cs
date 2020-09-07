1da8b55a291698097326cc02d913412e2ec293ef
Florabel Ituralde <67927764+florabelituralde@users.noreply.github.com>
2020-09-07 12:31:49 -0400

    Upload new files

---
+++ VetChat/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VetChat.Areas.Identity.Data;
+using VetChat.Data;
+
+[assembly: HostingStartup(typeof(VetChat.Areas.Identity.IdentityHostingStartup))]
+namespace VetChat.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VetChatContext>(options =>
+                options.UseSqlServer(
+                    context.Configuration.GetConnectionString("VetChatContextConnetion")));
+
+                services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<VetChatContext>();
+            });
+        }
+    }
+}
---
+++ VetChat/Data/VetChatContext.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using VetChat.Models;
+
+namespace VetChat.Data
+{
+    public partial class VetChatContext : DbContext
+    {
+        public VetChatContext()
+        {
+        }
+
+        public VetChatContext(DbContextOptions<VetChatContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AdminData> AdminData { get; set; }
+        public virtual DbSet<GroupData> GroupData { get; set; }
+        public virtual DbSet<UserInformation> UserInformation { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AdminData>(entity =>
+            {
+                entity.HasKey(e => e.AdminId)
+                    .HasName("PK_AdminID");
+
+                entity.Property(e => e.AdminId)
+                    .HasColumnName("AdminID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.GroupId).HasColumnName("GroupID");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Group)
+                    .WithMany(p => p.AdminData)
+                    .HasForeignKey(d => d.GroupId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_AdminData_GroupID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AdminData)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_AdminData_UserID");
+            });
+
+            modelBuilder.Entity<GroupData>(entity =>
+            {
+                entity.HasKey(e => e.GroupId)
+                    .HasName("PK_GroupID");
+
+                entity.Property(e => e.GroupId)
+                    .HasColumnName("GroupID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.GroupName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.GroupVisibility)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.GroupData)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_GroupData_UserID");
+            });
+
+            modelBuilder.Entity<UserInformation>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK_UserID");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("UserID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Branch)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.DateOfEntry).HasColumnType("date");
+
+                entity.Property(e => e.Ets)
+                    .HasColumnName("ETS")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Firstname)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Lastname)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
